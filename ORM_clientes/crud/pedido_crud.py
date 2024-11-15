97ab1ded9bf8e79f4254fdbfef1950e5ec8b55e3
yadhi633 <133727933+yadhi633@users.noreply.github.com>
2024-11-15 12:31:05 -0300

    Update pedido_crud.py
    cambios realizados para crud de pedidos

--- ORM_clientes/crud/pedido_crud.py
+++ ORM_clientes/crud/pedido_crud.py
@@ -1,1 +1,31 @@
 #Yadhira
+from sqlalchemy.orm import Session
+from models import Pedido
+from datetime import datetime
+
+def crear_pedido(db: Session, descripcion: str, total: float, cantidad_menus: int, cliente_id: int):
+    nuevo_pedido = Pedido(
+        descripcion=descripcion,
+        total=total,
+        cantidad_menus=cantidad_menus,
+        fecha_creacion=datetime.now(),
+        cliente_id=cliente_id
+    )
+    db.add(nuevo_pedido)
+    db.commit()
+    db.refresh(nuevo_pedido)
+    return nuevo_pedido
+
+def obtener_pedidos(db: Session):
+    return db.query(Pedido).all()
+
+def obtener_pedidos_por_cliente(db: Session, cliente_id: int):
+    return db.query(Pedido).filter(Pedido.cliente_id == cliente_id).all()
+
+def eliminar_pedido(db: Session, pedido_id: int):
+    pedido = db.query(Pedido).filter(Pedido.id == pedido_id).first()
+    if pedido:
+        db.delete(pedido)
+        db.commit()
+        return True
+    return False
